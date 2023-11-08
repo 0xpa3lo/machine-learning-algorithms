1eba587af4d646225264ef8707fd6f8f78948ee7
pa3lo <65485705+0xpa3lo@users.noreply.github.com>
2023-11-08 11:46:50 +0200

    Update multinomial_naive_bayes.py

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -4,9 +4,13 @@ import math
 class MultinomialNB:
     def __init__(self, articles_per_tag):
         self.train()
+        self.articles_per_tag = articles_per_tag
+        self.tags = articles_per_tag.keys()
 
         
     def train(self):
+        priori_per_tag = {tag: 0 for tag in tags}
+        
 
 
     def __get_likelihoods_per_tag(self):
