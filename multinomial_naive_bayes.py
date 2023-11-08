f64de67421bddcf7f037928bf4e3ef9b574e6e41
pa3lo <65485705+0xpa3lo@users.noreply.github.com>
2023-11-08 11:17:22 +0200

    Update multinomial_naive_bayes.py

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -1,1 +1,25 @@
+from collections import defaultdict
+import math
 
+class MultinomialNB:
+    def __init__(self, articles_per_tag):
+        self.train()
+
+        
+    def train(self):
+
+
+    def __get_likelihoods_per_tag(self):
+
+        
+
+    def predict(self, article):
+
+
+
+
+
+
+        
+        
+        
