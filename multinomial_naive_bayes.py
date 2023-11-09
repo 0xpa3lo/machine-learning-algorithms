5ddc7cf73ba0465b06746c9193ab4b524e0b41ca
pa3lo <65485705+0xpa3lo@users.noreply.github.com>
2023-11-09 10:01:32 +0200

    Update multinomial_naive_bayes.py

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -6,14 +6,27 @@ class MultinomialNB:
         self.train()
         self.articles_per_tag = articles_per_tag
         self.tags = articles_per_tag.keys()
+        self.priori_per_tag = {}
+        self.likelihoods_per_word_per_tag
 
         
     def train(self):
-        priori_per_tag = {tag: 0 for tag in tags}
-        
+        # calculate prior
+        articles_per_tag_map = {tag: len(self.articles_per_tag[tag]) for tag in self.tags}
+        self.priori_per_tag = {tag: articles_per_tag_map[tag] / sum(articles_per_tag_map.values())  for tag in articles_per_tag_map.keys()}
+        # calculate likelihoods
+        likelihood =  __get_likelihoods_per_tag()
+
 
 
     def __get_likelihoods_per_tag(self):
+        word_freq_per_tag  = defaultdict(lambda: defaultdict(int))
+        total_word_per_tag = 
+        for tag, articles in self.articles_per_tag.items():
+            for word in articles:
+                word_counts[word][tag] += 1
+        
+
 
         
 
