fdeaae3ca22f3c69ac273f02fc49d0825952ea62
pa3lo <65485705+0xpa3lo@users.noreply.github.com>
2023-11-09 14:58:05 +0200

    Update multinomial_naive_bayes.py

--- multinomial_naive_bayes.py
+++ multinomial_naive_bayes.py
@@ -2,35 +2,55 @@ from collections import defaultdict
 import math
 
 class MultinomialNB:
-    def __init__(self, articles_per_tag):
+    def __init__(self, articles_per_tag, alpha=1):
+        self.alpha = alpha
+        self.priors_per_tag = {}  
+        self.likelihood_per_word_per_tag = {}  
+        self.articles_per_tag = articles_per_tag 
+        self.tags = self.articles_per_tag.keys()  
         self.train()
-        self.articles_per_tag = articles_per_tag
-        self.tags = articles_per_tag.keys()
-        self.priori_per_tag = {}
-        self.likelihoods_per_word_per_tag
 
-        
     def train(self):
-        # calculate prior
-        articles_per_tag_map = {tag: len(self.articles_per_tag[tag]) for tag in self.tags}
-        self.priori_per_tag = {tag: articles_per_tag_map[tag] / sum(articles_per_tag_map.values())  for tag in articles_per_tag_map.keys()}
-        # calculate likelihoods
-        likelihood =  __get_likelihoods_per_tag()
-
-
-
-    def __get_likelihoods_per_tag(self):
-        word_freq_per_tag  = defaultdict(lambda: defaultdict(int))
-        total_word_per_tag = 
-        for tag, articles in self.articles_per_tag.items():
-            for word in articles:
-                word_counts[word][tag] += 1
-        
-
-
-        
+        # Counting the number of articles/documents per tag/class
+        tag_counts_map = {tag: len(self.articles_per_tag[tag]) for tag in self.tags}  
+        # Calculating the prior probability of each tag/class
+        self.priors_per_tag = {tag: tag_counts_map[tag] / sum(tag_counts_map.values()) for tag in tag_counts_map.keys()}  
+        # Calculating the likelihood of words given a tag/class
+        self.likelihood_per_word_per_tag = self.__get_word_likelihoods_per_tag()  
 
     def predict(self, article):
+        # Initializing the posterior probabilities with the prior probabilities
+        posteriors_per_tag = {tag: math.log(prior) for tag, prior in self.priors_per_tag.items()}  
+        for word in article:
+            for tag in self.tags:
+                # Updating the posterior probabilities based on the likelihood of observing the word given the tag/class
+                posteriors_per_tag[tag] = posteriors_per_tag[tag] + math.log(
+                    self.likelihood_per_word_per_tag[word][tag]
+                )
+        return posteriors_per_tag
+
+    def __get_word_likelihoods_per_tag(self):
+        # Initializing word frequencies per tag/class
+        word_frequencies_per_tag = defaultdict(lambda: {tag: 0 for tag in self.tags})  
+        # Initializing total word count per tag/class
+        total_word_count_per_tag = defaultdict(int)  
+        for tag in self.tags:
+            for article in self.articles_per_tag[tag]:
+                for word in article:
+                    # Counting word frequencies per tag/class
+                    word_frequencies_per_tag[word][tag] += 1  
+                    # Counting total words per tag/class
+                    total_word_count_per_tag[tag] += 1  
+
+        # Initializing word likelihoods per tag/class
+        word_likelihoods_per_tag = defaultdict(lambda: {tag: 0.5 for tag in self.tags})  
+        for word, tags_map in word_frequencies_per_tag.items():
+            for tag in tags_map.keys():
+                # Calculating the likelihood of words given a tag/class with Laplace smoothing
+                word_likelihoods_per_tag[word][tag] = (word_frequencies_per_tag[word][tag] + 1 * self.alpha) / (
+                    total_word_count_per_tag[tag] + 2 * self.alpha
+                    )
+        return word_likelihoods_per_tag
 
 
 
