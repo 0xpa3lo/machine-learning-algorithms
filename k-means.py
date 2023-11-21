98d89d90074cd53baaa183e7ad1929beff47214c
pa3lo <65485705+0xpa3lo@users.noreply.github.com>
2023-11-21 11:58:23 +0200

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -15,6 +15,40 @@ def get_k_means(user_feature_map, num_features_per_user, k):
 
     centroids = [Centroid(user_feature_map[inital_centroid_user])for inital_centroid_user in inital_centroid_users]
 
+    for i in range(10):
+        for uid, features in user_feature_map.items():
+            closest_centroid_distance = float("inf")
+            closest_centroid = None
+            for centroid in centroids:
+                distance = calc_manhattan_distance(centroid, uid, num_features_per_user)
+                closest_centroid_distance = distance
+                if closest_centroid_distance < distance:
+                    closest_centroid_distance = distance
+                    closest_centroid = centroid
+                else:
+                    closest_centroid = centroid
+            self.closest_users.add(uid)
+            
+        for centroid in centroid:
+            centroid.location = get_centroid_avg(centroid, self.closes_users)
+            self.closest_users.clear()
+    return [centroid.location for centroid in centroids]
+                
+def calc_manhattan_distance(centroid, user, num_features_per_user):
+    distance = 0
+    for x in num_features:
+        distance += abs(centroid[x] - user[x])
+    return distance
 
+def get_centroid_avg(centroid, closest_users, num_features_per_user):
+    centroid_avg_distance = [0 for i in num_features_per_user]
+    for user in closest_users:
+        for i in num_features_per_user:
+            centroid_avg_distance[i] += user[i]
+
+    for i in num_features_per_user:
+        centroid_avg_distance[i] = centroid_avg_distance[i] / num_features_per_user
+        
+    return centroid_avg_distance
     
-
+  
